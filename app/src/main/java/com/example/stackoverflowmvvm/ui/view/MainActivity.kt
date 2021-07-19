88cc3b0ee40564c1420e0003212eafc4c3fc97d0
Gunin <gunin2001@gmail.com>
2021-07-19 15:38:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stackoverflowmvvm/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stackoverflowmvvm.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stackoverflowmvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
